ccea661bbfd7792174e1a095cde098ff7dc71c10
surech <stefan@surech.ch>
2019-09-11 22:17:12 +0200

    stu: read user and events and print them out

--- src/main/java/ch/surech/chronos/chronosimporter/ChronosImporterApplication.java
+++ src/main/java/ch/surech/chronos/chronosimporter/ChronosImporterApplication.java
@@ -1,12 +1,18 @@
 package ch.surech.chronos.chronosimporter;
 
 import ch.surech.chronos.chronosimporter.service.AuthentificationService;
+import ch.surech.chronos.chronosimporter.service.CalendarService;
+import ch.surech.chronos.chronosimporter.service.GraphService;
+import com.microsoft.graph.models.extensions.Event;
+import com.microsoft.graph.models.extensions.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ChronosImporterApplication implements CommandLineRunner {
@@ -15,6 +21,12 @@ public class ChronosImporterApplication implements CommandLineRunner {
 
     @Autowired
     private AuthentificationService authentificationService;
+
+    @Autowired
+    private GraphService graphService;
+
+    @Autowired
+    private CalendarService calendarService;
 
     public static void main(String[] args) {
         SpringApplication.run(ChronosImporterApplication.class, args);
@@ -25,5 +37,16 @@ public class ChronosImporterApplication implements CommandLineRunner {
         authentificationService.signIn();
 
         LOGGER.info("Token: " + authentificationService.getAccessToken());
+
+        User user = graphService.getUser();
+        LOGGER.info("Welcome {}!", user.displayName);
+
+        List<Event> events = calendarService.getEvents();
+        for (Event event : events) {
+            System.out.println("Subject: " + event.subject);
+            System.out.println("  Organizer: " + event.organizer.emailAddress.name);
+            System.out.println("  Start: " + event.start);
+            System.out.println("  End: " + event.end);
+        }
     }
 }
---
+++ src/main/java/ch/surech/chronos/chronosimporter/service/CalendarService.java
@@ -0,0 +1,38 @@
+package ch.surech.chronos.chronosimporter.service;
+
+import com.microsoft.graph.models.extensions.Event;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.options.Option;
+import com.microsoft.graph.options.QueryOption;
+import com.microsoft.graph.requests.extensions.IEventCollectionPage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class CalendarService {
+
+    @Autowired
+    private GraphService graphService;
+
+    public List<Event> getEvents(){
+        IGraphServiceClient graphClient = graphService.getGraphClient();
+
+        // Use QueryOption to specify the $orderby query parameter
+        final List<Option> options = new LinkedList<Option>();
+        // Sort results by createdDateTime, get newest first
+        options.add(new QueryOption("orderby", "createdDateTime DESC"));
+
+        // GET /me/events
+        IEventCollectionPage eventPage = graphClient
+                .me()
+                .events()
+                .buildRequest(options)
+                .select("subject,organizer,start,end")
+                .get();
+
+        return eventPage.getCurrentPage();
+    }
+}
