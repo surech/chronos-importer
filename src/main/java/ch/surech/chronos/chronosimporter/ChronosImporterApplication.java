4a070f47ae0f052c5cbbdfec54c1a0b39c4ed353
surech <stefan@surech.ch>
2019-09-11 21:28:37 +0200

    stu: Implement Authentification

--- src/main/java/ch/surech/chronos/chronosimporter/ChronosImporterApplication.java
+++ src/main/java/ch/surech/chronos/chronosimporter/ChronosImporterApplication.java
@@ -1,5 +1,9 @@
 package ch.surech.chronos.chronosimporter;
 
+import ch.surech.chronos.chronosimporter.service.AuthentificationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChronosImporterApplication implements CommandLineRunner {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChronosImporterApplication.class);
+
+    @Autowired
+    private AuthentificationService authentificationService;
+
     public static void main(String[] args) {
         SpringApplication.run(ChronosImporterApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Los geht's!!");
+        authentificationService.signIn();
+
+        LOGGER.info("Token: " + authentificationService.getAccessToken());
     }
 }
